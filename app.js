256af10d6fdd4bf516801f426e6af4fdb5d9b1c5
Toniopoina <guilherme.poina@gmail.com>
2025-03-12 15:30:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
 const app = express();
 
 
 app.use(express.static("public"))
 app.use(express.json());
-app.use(express.urlencoded({ extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -16,8 +16,34 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
-})
+});
 
-app.listen(80, ()=>{
+app.getMaxListeners("/pokemons", async (request, response) => {
+    const result = await pool.query("SELECT * FROM pokemons ORDER BY id");
+    response.json(result, rows);
+});
+
+app.post("/pokemons", async (request, response) => {
+    const { nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2 } = request.body;
+    const result = await pool.query("INSERT INTO pokemons (nome,peso, ataque,defesa,velocidade, habilidade1, habilidade2) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", [nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2]);
+    response.json(result.rows[0]);
+});
+
+app.put("/pokemons/:id", async (request, response) => {
+    const id = request.params.id;
+    const { nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2 } = resquest.body;
+    const result = await pool.query("UPDATE pokemons SET nome=$1, peso=$2, ataque=$3, defesa=$4, velocidade=$5, habilidade1=$6, habilidade2=$7 WHERE id=$8 RETURNING *", [nome, peso, ataque, defesa, velocidade, habilidade1, habilidade2, id]);
+    response.json(result.rows[0]);
+
+});
+
+app.delete("/pokemons/:id", async (request, response) => {
+    const id = request.params.id;
+    const result = await pool.query("DELETE * FROM pokemons WHERE id=$1", [id]);
+    response.json(result.rows[0]);
+});
+
+app.listen(80, () => {
     console.log("Server is running on port 80")
-})
+});
+
